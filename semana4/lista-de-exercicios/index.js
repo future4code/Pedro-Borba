177f2ed55eefad3452c312cb2c122b880b00547b
Pedro Borba <pedro_borba77@hotmail.com>
2020-12-13 18:14:41 -0300

    exercício 1 resposta: Convesão de dolar x real

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,12 @@
+// exercício 1 resposta:
+// Basicamente o execício vai converter o meu valor em dolar vai multiplicar com a cotação e vai me passar o valor em real.
+
+//Execício 2 resposta:
+// Esse exercício tem várias opções de investimento e a cada opção que você escolhe ele te retorna o valor de investimento marcado com a cotação e o valor se será retonado com esse investimento. 
+
+
+//Execício 3 Resposta:
+// Existe 3 contantes uma com uma lista de numeros e outras duas vazias
+// meu for esta recebendo uma variavel numero de numeros que foi uma constante criada,
+// se os numeros da minha lista acima forem divisiveis por dois esse irão ficar na segunda contante criada, se não
+//e las irão ficar na tereceira contante criada, basicamente, o exercicio é para separa numeros pares e impares.
